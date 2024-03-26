bd5874d7c73d449931a864d06012929c61b807c6
sowmya_codes <sowmya.reachout@gmail.com>
2024-03-26 14:49:07 +0530

    Day 2

---
+++ Practice/Array/BestTimeToBuyAndSell.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class BestTimeToBuyAndSell
+{
+    public static int solution(int [] prices)
+    {
+        int maxProfit = 0;
+        int minBuy = prices[0];
+        for(int i = 1; i< prices.length; i++)
+        {
+            int profit = prices[i] - minBuy;
+            maxProfit = Math.max(maxProfit, profit);
+            minBuy = Math.min(minBuy, prices[i]);
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) 
+    {
+        int [] prices = {7,1,5,3,6,4};
+        System.out.println(solution(prices));
+    }
+}
